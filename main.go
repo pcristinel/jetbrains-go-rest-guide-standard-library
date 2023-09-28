ff7fab9894891203a347a53fbd007ecb2e6227bf
Cristinel Pavel Alergus <cristinelpavelalergus@gmail.com>
2023-09-28 16:12:18 +0200

    Root path responding

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const PORT = 8080
+
+type HomeHandler struct {
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", HomeHandler{})
+
+	// Run the server
+	err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), mux)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Println(fmt.Sprintf("Listening on port %v", PORT))
+}
+
+func (h HomeHandler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	_, err := w.Write([]byte("This is my home page"))
+	if err != nil {
+		log.Println("Error while writing to request: ", err)
+		return
+	}
+}
